9d6456e1f84ea2334030ed91fe44c6e0f7788b23
Ayush Gurjar <jiaayush256@gmail.com>
2024-11-06 17:30:26 +0000

    dataTypes-summary

--- basics/04_comparision.js
+++ basics/04_comparision.js
@@ -11,7 +11,6 @@ console.log(2 != 1)
 //console.log("02" > 1)
 
 
-
 //avoid these types of conversions
 //console.log(null > 0) //false
 //console.log(null == 0) //false
@@ -19,5 +18,5 @@ console.log(2 != 1)
 
 //avoid these types of conversions
 //console.log(undefined > 0) //false
-console.log(undefined < 0) //false
-console.log(undefined == 0) //false
+//console.log(undefined < 0) //false
+//console.log(undefined == 0) //false
--- basics/conversionOperations.js
+++ basics/conversionOperations.js
@@ -22,5 +22,40 @@ let booleanIsloggedIn = Boolean(isloggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+
+let value = 5
+let negativeValue = -value
+//console.log(negativeValue);
+
+
+//operations
+/*
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/2)
+console.log(2%2)
+*/
+
+let str1 = "hello"
+let str2 = " ayush"
+
+let str3 = str1 + str2
+//console.log(str3)
+
+/*
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+*/
+
+//console.log(+true)
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
