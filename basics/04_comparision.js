86b23a47daf6644ab074aff59e5c1b0e64a4e4c9
Ayush Gurjar <jiaayush256@gmail.com>
2024-11-06 15:36:54 +0000

    comparisions

---
+++ basics/04_comparision.js
@@ -0,0 +1,23 @@
+/*
+console.log(2 > 1)
+console.log(2 >= 1)
+console.log(2 < 2)
+console.log(2 == 1)
+console.log(2 != 1)
+*/
+
+
+//console.log("2" > 1)
+//console.log("02" > 1)
+
+
+
+//avoid these types of conversions
+//console.log(null > 0) //false
+//console.log(null == 0) //false
+//console.log(null >= 0) //true
+
+//avoid these types of conversions
+//console.log(undefined > 0) //false
+console.log(undefined < 0) //false
+console.log(undefined == 0) //false
